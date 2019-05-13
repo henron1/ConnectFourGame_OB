342d843ff7e0bb83119ce40c5fca6638e9e38015
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-05-13 19:41:04 -0400

    cells are rendering! need to adjust css to take shape of grid instead of 1 column

--- connect4/src/components/Cell.js
+++ connect4/src/components/Cell.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import "./Cell.css"
 
 export default function Cell(props) {
-    if (props.cellValue === true) {
-        const chip = <img src={props.cellValue} alt="player" />
-    } else {
-        null
-    }
+    // if (props.cellValue === true) {
+    //     const chip = <img src={props.cellValue} alt="player" />
+    // } else {
+    //     null
+    // }
+    // refactor to ternary...no error
+    const chip = props.value ? <img src={props.value} alt="player" /> : null
 
-    return (
+    return(
         <div className="cell">
             <div className="chip">{chip}</div>
         </div>
--- connect4/src/components/game.js
+++ connect4/src/components/game.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import player1 from "../images/react.png"
 import player2 from "../images/redux.png"
+import CellSet from "./CellSet"
 
 // const column = new Array(7).fill(0)
 // const row = new Array(6).fill(0)
@@ -8,31 +9,36 @@ import player2 from "../images/redux.png"
 const p1 = player1
 const p2 = player2
 
-
-
 export default class Game extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            colRow = new Array(7).fill(0).map(() => new Array(6).fill(0)),
+            board : new Array(7).fill(0).map(() => new Array(6).fill(null)),
             gameOver: null,
-            firstPlayer:[p1],
+            firstPlayer:[p1, null, null],
         }
     }
 
-    cellRender(){
+    // pass in column number into the state object
+    cellRender(colNum){
         return(
-            <CellSet>
-                
-            </CellSet>
+            <CellSet value={this.state.board[colNum]} />
         )
     }
 
+    
+    // render each column start cell that propogates row down below it!
     render() {
         return (
             <>
-                <div>
-
+                <div className="gameBoard">
+                    {this.cellRender(0)}
+                    {this.cellRender(1)}
+                    {this.cellRender(2)}
+                    {this.cellRender(3)}
+                    {this.cellRender(4)}
+                    {this.cellRender(5)}
+                    {this.cellRender(6)}
                 </div>
             </>
         )
